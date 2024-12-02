acdfeaf1845876cb70a131659c75fe685aa5fd63
Bennett Zhu <bzhu@confluent.io>
2024-12-02 13:23:25 -0800

    initial setup

---
+++ tools.go
@@ -0,0 +1,8 @@
+package tools
+
+import (
+	_ "github.com/bennettzhu1/bazel_go_demo/libs/hello"
+	_ "github.com/bennettzhu1/bazel_go_demo/services/one"
+	_ "github.com/google/uuid"
+	_ "github.com/labstack/echo/v4"
+)
